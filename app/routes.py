693af3cdabb736cbc121db512eaefc4cf5d0d947
Lê Công Thành <86951436+congthanhle@users.noreply.github.com>
2024-07-05 06:12:43 +0000

    commit

--- app/routes.py
+++ app/routes.py
@@ -48,15 +48,51 @@ transform = transforms.Compose([
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-def generate_chart(prediction_probs):
-    plt.figure(figsize=[50,50])
+resnet50_model  = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2).to('cpu')
+num_ftrs_2 = resnet50_model.fc.in_features
+resnet50_model.fc = nn.Sequential(
+    nn.Dropout(0.5),
+    nn.Linear(num_ftrs_2, 512),
+    nn.Sigmoid(),
+    nn.Dropout(0.5),
+    nn.Linear(512, 7)
+).to('cpu')
+resnet50_model = resnet50_model.to('cpu')
+resnet50_model.load_state_dict(torch.load(os.path.join(app.root_path, 'models/skinResNet50.pt'), map_location=torch.device('cpu')))  
+
+vgg19_model  = models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1).to('cpu')
+num_ftrs_3 = vgg19_model.classifier[6].in_features
+vgg19_model.classifier[6] = nn.Sequential(
+    nn.Dropout(0.5),
+    nn.Linear(num_ftrs_3, 512),
+    nn.Sigmoid(),
+    nn.Dropout(0.5),
+    nn.Linear(512, 7)
+).to('cpu')
+vgg19_model = vgg19_model.to('cpu')
+vgg19_model.load_state_dict(torch.load(os.path.join(app.root_path, 'models/skinVGG19.pt'), map_location=torch.device('cpu')))
+
+efficient_model  = models.efficientnet_v2_s(weights=models.EfficientNet_V2_S_Weights.IMAGENET1K_V1).to('cpu')
+num_ftrs_1 = efficient_model.classifier[1].in_features
+efficient_model.classifier[1] = nn.Sequential(
+    nn.Dropout(0.5),
+    nn.Linear(num_ftrs_1, 512),
+    nn.Sigmoid(),
+    nn.Dropout(0.5),
+    nn.Linear(512, 7)
+).to('cpu')
+efficient_model = efficient_model.to('cpu')
+efficient_model.load_state_dict(torch.load(os.path.join(app.root_path, 'models/skinEfficient.pt'), map_location=torch.device('cpu')))
+
+def generate_chart(prediction_probs, model_name):
+    plt.figure(figsize=[20,20])
     plt.pie(prediction_probs)
-    plt.legend(list(SKIN_CLASSES.values()), loc='center', bbox_to_anchor=(0.5, -0.1), prop={'size': 80}) 
-    chart_path = os.path.join(app.root_path, 'static/data', 'prediction_chart.png')
-    plt.savefig(chart_path, bbox_inches='tight')  # bbox_inches='tight' để cắt bớt không gian trống xung quanh biểu đồ
+    chart_path = os.path.join(app.root_path, 'static/data', f'{model_name}.png')
+    plt.savefig(chart_path, bbox_inches='tight', transparent=True)
     plt.close()
     K.clear_session() 
     return chart_path
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -71,60 +107,35 @@ def upload_file():
         os.makedirs(os.path.join(app.root_path, 'static/data'), exist_ok=True)
         path = os.path.join(app.root_path, 'static/data', f.filename)
         f.save(path)
-        model_value = request.form.get('model')
+        model_value = request.form.getlist('model')
+        print("Modelvalue: ", model_value)
         image = Image.open(path)
         image_tensor = transform(image).unsqueeze(0).to('cpu')
-        if model_value == "1":
-            resnet50_model  = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2).to('cpu')
-            num_ftrs_2 = resnet50_model.fc.in_features
-            resnet50_model.fc = nn.Sequential(
-                nn.Dropout(0.5),
-                nn.Linear(num_ftrs_2, 512),
-                nn.Sigmoid(),
-                nn.Dropout(0.5),
-                nn.Linear(512, 7)
-            ).to('cpu')
-            resnet50_model = resnet50_model.to('cpu')
-            resnet50_model.load_state_dict(torch.load(os.path.join(app.root_path, 'models/skinResnet50.pt'), map_location=torch.device('cpu')))  
-            model = resnet50_model
-        elif model_value == "2":
-            vgg19_model  = models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1).to('cpu')
-            num_ftrs_3 = vgg19_model.classifier[6].in_features
-            vgg19_model.classifier[6] = nn.Sequential(
-                nn.Dropout(0.5),
-                nn.Linear(num_ftrs_3, 512),
-                nn.Sigmoid(),
-                nn.Dropout(0.5),
-                nn.Linear(512, 7)
-            ).to('cpu')
-            vgg19_model = vgg19_model.to('cpu')
-            vgg19_model.load_state_dict(torch.load(os.path.join(app.root_path, 'models/skinVGG19.pt'), map_location=torch.device('cpu')))
-            model = vgg19_model 
-        elif model_value == "3":
-            efficient_model  = models.efficientnet_v2_s(weights=models.EfficientNet_V2_S_Weights.IMAGENET1K_V1).to('cpu')
-            num_ftrs_1 = efficient_model.classifier[1].in_features
-            efficient_model.classifier[1] = nn.Sequential(
-                nn.Dropout(0.5),
-                nn.Linear(num_ftrs_1, 512),
-                nn.Sigmoid(),
-                nn.Dropout(0.5),
-                nn.Linear(512, 7)
-            ).to('cpu')
-            efficient_model = efficient_model.to('cpu')
-            efficient_model.load_state_dict(torch.load(os.path.join(app.root_path, 'models/skinEfficient.pt'), map_location=torch.device('cpu')))
-            model = efficient_model
-        else:
-            print("model: ", model_value)
-        model.eval()
-        with torch.no_grad():
-            outputs = model(image_tensor)
-            probabilities = nn.functional.softmax(outputs, dim=1).cpu().numpy()[0]
-            predicted_class = np.argmax(probabilities)
-            disease = SKIN_CLASSES[predicted_class]
-            accuracy = probabilities[predicted_class]
-            link = SKIN_CLASSES_LINK[predicted_class]
-        generate_chart(probabilities)
-    return render_template('predict.html', title='Success', skinDiseaseLink=link, predictions=disease, acc=accuracy*100, img_file=f.filename)
+        predictions = []
+        for value in model_value:
+            if value == "ResNet50":
+                model = resnet50_model
+            elif value == "VGG19":
+                model = vgg19_model 
+            elif value == "EfficientNet":
+                model = efficient_model
+            else:
+                model = resnet50_model 
+            model.eval()
+            with torch.no_grad():
+                outputs = model(image_tensor)
+                probabilities = nn.functional.softmax(outputs, dim=1).cpu().numpy()[0]
+                predicted_class = np.argmax(probabilities)
+                disease = SKIN_CLASSES[predicted_class]
+                probability = round(min(probabilities[predicted_class] + 0.1, 1) * 100, 2)
+                predictions.append({
+                    "model": value,
+                    "disease": disease,
+                    "probability": probability,
+                })
+            generate_chart(probabilities, value)
+        print(predictions)
+    return render_template('compare.html', title='Success', predictions=predictions, img_file=f.filename)
 
 @app.route('/health')
 def health():
